andinus/crux

commit f3d1bc5f9d86300c42d6a57a9f60bd508bdf9bb4

Use URI to encode the url for random_search
I'll use this for everything eventually. Before this search terms like
"rocky mountains" would break, also segments, query_keywords makes it
easier.

lib/UnsplashSource.pm
@@ -5,6 +5,7 @@ package UnsplashSource;
 use strict;
 use warnings;
 
+use URI;
 use HTTP::Tiny;
 use Carp qw( croak carp );
 
@@ -42,11 +43,16 @@ sub get_random {
 
 sub random_search {
     my ( %options ) = @_;
-    my $url = "$api/";
-    $url .= "featured/" if $options{featured};
-    $url .= $options{resolution};
-    $url .= "?";
-    $url .= "$_," foreach ( @{$options{search}});
+
+    my $url = URI->new($api);
+
+    my @segments;
+    push @segments, "featured" if $options{featured};
+    push @segments, $options{resolution};
+    $url->path_segments( @segments );
+
+    $url->query_keywords( \@{$options{search}} );
+
     return $http->head($url);
 }
 

commit 8dd9f42bfae6bb62b93b91f9c47ff410b12fa58b

Fix logical error in $url generation
This issue started with f3d1bc5f9d86300c42d6a57a9f60bd508bdf9bb4:

  Use URI to encode the url for random_search

  I'll use this for everything eventually. Before this search terms
  like "rocky mountains" would break, also segments, query_keywords
  makes it easier.

Now the problem was that query_keywords forms urls like
`?nature+water' which totally makes sense & this is why it's logical
error. I want to form it in `?nature,water' format so first we use
`join' to form `nature,water' & then use URI::Encode to encode it
correctly & then use query_keywords to form $url.

We have to use URI::Encode because otherwise we would end up with same
problem that I tried fixing in
f3d1bc5f9d86300c42d6a57a9f60bd508bdf9bb4. "rocky mountains" would've
been seperated by space.

Now it's all good, every space will be encoded & keywords will be
seperated by `,' which is what Unsplash Source wants.

lib/UnsplashSource.pm
@@ -8,6 +8,7 @@ use warnings;
 use URI;
 use HTTP::Tiny;
 use Carp qw( croak carp );
+use URI::Encode qw( uri_encode );
 
 my $api = "https://source.unsplash.com";
 my $http = HTTP::Tiny->new(
@@ -51,7 +52,7 @@ sub random_search {
     push @segments, $options{resolution};
     $url->path_segments( @segments );
 
-    $url->query_keywords( \@{$options{search}} );
+    $url->query_keywords( uri_encode(join(',', @{$options{search}})) );
 
     return $http->head($url);
 }
@@ -92,7 +93,7 @@ sub fixed {
     push @segments, "weekly" if $options{weekly};
     $url->path_segments( @segments );
 
-    $url->query_keywords( \@{$options{search}} );
+    $url->query_keywords( uri_encode(join(',', @{$options{search}})) );
 
     return $http->head($url);
 }